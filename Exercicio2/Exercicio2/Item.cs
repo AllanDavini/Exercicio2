39c5ab791a4cc92bed86b5a775c830511ac8c5da
AllanDavini <allan_mv19@hotmail.com>
2017-12-02 01:46:02 -0200

    exe2

---
+++ Exercicio2/Exercicio2/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercicio2
+{
+    public class Item
+    {
+        public Item(int id, string nome, double preco, int validade, double custoCompra, int volumeOcupado, bool refrigerado, int riscoVencer)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Validade = validade;
+            CustoCompra = custoCompra;
+            VolumeOcupado = volumeOcupado;
+            Refrigerado = refrigerado;
+            RiscoVencer = riscoVencer;
+            Custo = CalcularCusto();
+        }
+        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Custo { get; set; }
+        public double Preco { get; set; }
+        public int Validade { get; set; }
+        public double CustoCompra { get; set; }
+        public int VolumeOcupado { get; set; }
+        public bool Refrigerado { get; set; }
+        public int RiscoVencer { get; set; }
+
+        internal double Formula(double custo, int validade)
+        {
+            if (validade < 5)
+            {
+                return custo * 1.5;
+            }
+
+            return custo * 3;
+        }
+
+        private double CalcularCusto()
+        {
+            var custo = Refrigerado
+                ? (CustoCompra * VolumeOcupado) * 1.25 / 10
+                : (CustoCompra * VolumeOcupado) / 10;
+
+            switch (RiscoVencer)
+            {
+                case 1:
+                    return custo * 2;
+                case 2:
+                    return custo * 1.75;
+                case 3:
+                    return custo * 1.50;
+                case 4:
+                    return custo * 1.25;
+                default:
+                    return custo;
+            }
+
+        }
+    }
+}
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var listItens = new List<Item>
+            {
+                new Item(1, "Coca-Cola", 0, 365, 1, 10, true, 1),
+                new Item(2, "Salgadinho Doritos", 0, 35, 2, 20, false, 2),
+                new Item(3, "Salame", 0, 2, 10, 5, false, 5),
+                new Item(4, "Pão de Forma", 0, 4, 1, 5, false, 3),
+                new Item(5, "Bolacha", 0, 20, 1, 20, false, 2)
+            };
+
+            foreach (var iten in listItens)
+            {
+                iten.Preco = iten.Formula(iten.Custo, iten.Validade);
+                Console.WriteLine($"{iten.Id} - {iten.Nome} - R${iten.Preco}");
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
